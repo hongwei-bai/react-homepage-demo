61b28a925363e19fc95897b8a2a2ac58ca6f6570
Hongwei Bai <bhw8412@hotmail.com>
2020-03-29 20:01:50 +1100

    add auto-scaled background image

--- src/App.js
+++ src/App.js
@@ -6,25 +6,33 @@ import {Button} from '@material-ui/core';
 function App() {
     return (
         <div className="App">
-            <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"/>
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    -- 欢迎访问白宏伟的网站！ --<br/><br/>
-                    -- Welcome to Hongwei's Home Page --
-                    {/*Edit <code>src/App.js</code> and save to reload.*/}
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-                <br/><br/>
-                <Button variant="contained" color="primary" href="http://www.beian.miit.gov.cn">京ICP备20008547号-1</Button>
-            </header>
+            <link rel="stylesheet"
+                  href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"/>
+            <div className="PageHeader"/>
+            <div className="AppBackground">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"
+                         style={{backgroundSize: 'cover', backgroundRepeat: 'no-repeat', zIndex: '-2'}}/>
+                    <p>
+                        -- 欢迎访问白宏伟的网站！ --<br/><br/>
+                        -- Welcome to Hongwei's Home Page --
+                        {/*Edit <code>src/App.js</code> and save to reload.*/}
+                    </p>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+                    </a>
+                    <br/><br/>
+                </header>
+            </div>
+            <div className="PageFooter">
+                <a align="right" className="CopyRightLink" href="http://www.beian.miit.gov.cn" target="_blank"
+                   rel="noopener noreferrer">互联网ICP备案号: 京ICP备20008547号-1</a>
+            </div>
         </div>
     );
 }
