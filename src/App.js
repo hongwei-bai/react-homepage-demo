bf7230d9b947dd83d8f1c1d5e38c104515942bca
Hongwei Bai <bhw8412@hotmail.com>
2020-04-05 14:43:01 +1000

    new home page v2

--- src/App.js
+++ src/App.js
@@ -1,38 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
+import bannerBg from './images/space.jpg';
 import './App.css';
-import {Button} from '@material-ui/core';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import {Button} from 'react-bootstrap';
+import Form from "react-bootstrap/Form";
 
 function App() {
     return (
         <div className="App">
             <link rel="stylesheet"
                   href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"/>
-            <div className="PageHeader"/>
-            <div className="AppBackground">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"
+            <link href="https://fonts.googleapis.com/css2?family=Oleo+Script&display=swap" rel="stylesheet"/>
+            <link href="https://fonts.googleapis.com/css2?family=ZCOOL+XiaoWei&display=swap" rel="stylesheet"/>
+            <link href="https://fonts.googleapis.com/css2?family=Abel&display=swap" rel="stylesheet"/>
+            <ul>
+                <li className="Banner">
+                    <img src={bannerBg} alt="banner"
                          style={{backgroundSize: 'cover', backgroundRepeat: 'no-repeat', zIndex: '-2'}}/>
-                    <p>
-                        -- 欢迎访问白宏伟的网站！ --<br/><br/>
-                        -- Welcome to Hongwei's Home Page --
-                        {/*Edit <code>src/App.js</code> and save to reload.*/}
-                    </p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
-                    <br/><br/>
-                </header>
-            </div>
-            <div className="PageFooter">
-                <a align="right" className="CopyRightLink" href="http://www.beian.miit.gov.cn" target="_blank"
-                   rel="noopener noreferrer">互联网ICP备案号: 京ICP备20008547号-1</a>
-            </div>
+                    <h1>Welcome</h1>
+                </li>
+                <li className="Main">
+                    <Form className="Login">
+                        <Form.Group className="FormGroupUsername" controlId="formUsername">
+                            <Form.Control type="username" placeholder="Username/Guest code"/>
+                            <Button variant="primary" type="submit">Go</Button>
+                        </Form.Group>
+                    </Form>
+                </li>
+                <li className="Footer">
+                    <a align="right" href="http://www.beian.miit.gov.cn" target="_blank"
+                       rel="noopener noreferrer">互联网ICP备案号: 京ICP备20008547号-1</a>
+                </li>
+            </ul>
         </div>
     );
 }
