cf9dd4692acddbcc35aa41f9598f10328706e576
Hongwei Bai <bhw8412@hotmail.com>
2020-05-14 22:51:36 +1000

    add loading

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -9,18 +9,24 @@ import imgCovid19 from './images/covid-19-coronavirus-epidemic-wuhan-2019-ncov.j
 import imgExercise from './images/man-running.jpg';
 import imgFlight from './images/boeing-737-cockpit.jpg';
 
+import Cookies from 'universal-cookie';
+
 const dataDashboard = {
     covid19: {image: imgCovid19, title: "Coronavirus(COVID-19)"},
     exercise: {image: imgExercise, title: "Exercise"},
     flightsim: {image: imgFlight, title: "Flight Simulation"}
 };
 
+const cookies = new Cookies();
+
 const flightUpdate = [
     "CYCLE 1801", <br/>,
     "ADEP ", <a href="https://www.crc.id.au/xplane/charts/DAPS-2020-FEB-27/Canberra%20(YSCB).pdf">YSCB🔗</a>, <br/>,
     "DEPRWY RW35", <br/>,
     "SID CULIN9", <br/>,
-    "ADES ", <a href="https://www.crc.id.au/xplane/charts/DAPS-2020-FEB-27/Sydney%20Kingsford%20Smith%20(YSSY).pdf">YSSY🔗</a>, <br/>,
+    "ADES ",
+    <a href="https://www.crc.id.au/xplane/charts/DAPS-2020-FEB-27/Sydney%20Kingsford%20Smith%20(YSSY).pdf">YSSY🔗</a>,
+    <br/>,
     "DESRWY RW07", <br/>,
     "STAR ODALE6"
 ]
@@ -29,13 +35,18 @@ class Dashboard extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            loaded: false,
             dataCovid19: {
                 Australia: {
                     NewConfirmed: 0,
                     NewDeaths: 0,
                     Confirmed: 0,
                     Deaths: 0,
-                    Tests: 0
+                    Tests: 0,
+                    Date: "",
+                    Time: "",
+                    NSW: 0,
+                    VIC: 0
                 },
                 China: {
                     NewConfirmed: 0,
@@ -89,6 +100,34 @@ class Dashboard extends React.Component {
             redirect: 'follow'
         };
 
+        this.setState({loaded: false})
+        let AuDate = "";
+        let AuTime = "";
+        let NSWCases = 0;
+        let VICCases = 0;
+
+        fetch("http://hongwei-test.top/hongwei-homepage-service/covid19/querybystate.do?days=1", requestOptions)
+            .then(response => response.json())
+            .then(
+                result => {
+                    AuDate = result['ausDataByStatePerDays'][0]['date'];
+                    AuTime = result['ausDataByStatePerDays'][0]['timeFrom'];
+                    NSWCases = result['ausDataByStatePerDays'][0]['NSW'];
+                    VICCases = result['ausDataByStatePerDays'][0]['VIC'];
+                    this.fetchWorld(AuDate, AuTime, NSWCases, VICCases)
+                }
+            )
+            .catch(error => {
+                console.log('error', error)
+                this.fetchWorld("API not available!", "", 0, 0)
+            });
+    }
+
+    fetchWorld(lAuDate, lAuTime, lNSWCases, lVICCases) {
+        const requestOptions = {
+            method: 'GET',
+            redirect: 'follow'
+        };
         fetch("https://corona.lmao.ninja/v2/countries?sort=country", requestOptions)
             .then(response => response.json())
             .then(
@@ -106,7 +145,11 @@ class Dashboard extends React.Component {
                             NewDeaths: dataAustralia['todayDeaths'],
                             Confirmed: dataAustralia['cases'],
                             Deaths: dataAustralia['deaths'],
-                            Tests: dataAustralia['tests']
+                            Tests: dataAustralia['tests'],
+                            Date: lAuDate,
+                            Time: lAuTime,
+                            NSW: lNSWCases,
+                            VIC: lVICCases
                         },
                         China: {
                             NewConfirmed: dataChina['todayCases'],
@@ -151,17 +194,93 @@ class Dashboard extends React.Component {
                             Tests: dataSingapore['tests']
                         }
                     }
+                    this.setState({loaded: true})
                     this.setState({dataCovid19: data});
+                    this.render();
                 }
             )
             .catch(error => console.log('error', error));
     }
 
     componentDidMount() {
+        // cookies.set('myCat', 'Pacman', { path: '/' });
+        console.log(cookies.get('myCat')); // Pacman
+
         this.getSummary();
     }
 
     render() {
+        if (!this.state.loaded) {
+            return (
+                <ul className="Dashboard">
+                    <li>
+                        <Card style={{
+                            width: '18rem',
+                            height: '100%',
+                            marginLeft: 'auto',
+                            marginRight: 'auto',
+                            marginTop: '0.1em',
+                            marginBottom: '0.1em'
+                        }}>
+                            <Card.Img variant="top" src={dataDashboard.covid19.image}/>
+                            <Card.Body>
+                                <Card.Title>{dataDashboard.covid19.title}</Card.Title>
+                                <Card.Text>
+                                    Loading...
+                                </Card.Text>
+                                <Button variant="primary">Details</Button>
+                            </Card.Body>
+                        </Card>
+                    </li>
+                    <li>
+                        <Card style={{
+                            width: '18rem',
+                            height: '100%',
+                            marginLeft: 'auto',
+                            marginRight: 'auto',
+                            marginTop: '0.1em',
+                            marginBottom: '0.1em'
+                        }}>
+                            <Card.Img variant="top" src={dataDashboard.exercise.image}/>
+                            <Card.Body>
+                                <Card.Title>{dataDashboard.exercise.title}</Card.Title>
+                                <Card.Text>
+                                    No data!
+                                </Card.Text>
+                                <Button variant="primary">Details</Button>
+                            </Card.Body>
+                        </Card>
+                    </li>
+                    <li>
+                        <Card style={{
+                            width: '18rem',
+                            height: '100%',
+                            marginLeft: 'auto',
+                            marginRight: 'auto',
+                            marginTop: '0.1em',
+                            marginBottom: '0.1em'
+                        }}>
+                            <Card.Img variant="top" src={dataDashboard.flightsim.image}/>
+                            <Card.Body>
+                                <Card.Title>{dataDashboard.flightsim.title}</Card.Title>
+                                <Card.Text>
+                                    {flightUpdate}
+                                </Card.Text>
+                                <Button variant="primary">Details</Button>
+                            </Card.Body>
+                        </Card>
+                    </li>
+                </ul>
+            )
+        }
+        let NSWStr = this.state.dataCovid19.Australia.NSW
+        let VICStr = this.state.dataCovid19.Australia.VIC
+        let byStateCaption = "(by " + this.state.dataCovid19.Australia.Date + " " + this.state.dataCovid19.Australia.Time + ")"
+        if (this.state.dataCovid19.Australia.Time == "") {
+            NSWStr = "-"
+            VICStr = "-"
+            byStateCaption = ""
+        }
         return (
             <ul className="Dashboard">
                 <li>
@@ -188,6 +307,11 @@ class Dashboard extends React.Component {
                                     {this.state.dataCovid19.Australia.Deaths}/
                                     <span className="Test">{this.state.dataCovid19.Australia.Tests}</span>
                                     <br/>
+                                    <nbsp/>
+                                    -
+                                    NSW:{NSWStr} {byStateCaption}<br/>
+                                    <nbsp/>
+                                    - Victoria:{VICStr}<br/>
                                     China:
                                     <span className="Today">+{this.state.dataCovid19.China.NewConfirmed}/+
                                         {this.state.dataCovid19.China.NewDeaths}</span>/
